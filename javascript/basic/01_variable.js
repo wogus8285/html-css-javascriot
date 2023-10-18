bc83c55712850181d4e2e3baf84c90e7acb96ed4
wogus8285 <wogus8285@gmail.com>
2023-10-18 14:40:43 +0900

    d

---
+++ javascript/basic/01_variable.js
@@ -0,0 +1,150 @@
+// Variable 변수
+// var, let, const
+
+var username = 'Alice'
+var userage = 25
+var useremali = 'alice@gmail.com'
+
+// 변수명 규칙
+// $ _ 석으면 됨
+// 아ㄹ파벳도 당능
+// 그런데 가끔 저작권 침해 당할수도
+
+
+
+// console.log(username)
+// comnole.log(userage)
+// comnole.log(userem)
+
+
+// 11,..   버 불록을 무시하낟
+// ㅣㄷㅅ 샤
+// 아니네밍 ; ㅗ뮫ㄱ
+
+// ㅍㄷㄱ  XMRQLDD LDLFTRL tmRLRK RKSDMGKEKEK일깃와 쓰기가 가능하다.$
+//  //2ㅣ ㅐㅇㄳ
+// -
+// -
+// -
+// -
+// --
+// ㅣㅅ ㅛㅑㅛ기ㄸ         -
+
+// -
+
+
+// ㅜ챠ㅏ앻 타이틀컨트률 세입 ㅎ유휴ㅡ 
+
+// 성불ㄹ고 숱먀ㅜ
+
+//  랴ㅜ샤ㅓㅜ 블ㄹ헛 쇼ㅡㅊ커크
+
+
+
+//  const
+
+//  {
+//     const subjexr = 'html'
+//     subject = css
+
+// 컨트섵트 아쩌굴 -> 강수 앴다. 절배 변하하지 않는다.$
+
+
+
+//  현업에선 comst를 주러 씀
+
+
+
+{
+    const name1 = 'Alice'
+    const name2 = 'alice'
+
+    console.log(name1, name2)
+
+    const firstName = 'John'
+    const lastName = 'Snow'
+    const fullName = firstName + ' '  + lastName
+
+    console.log(fullName)
+    console.log(`${firstName}  ${lastName}`)
+}
+
+
+// 숫자형 
+
+{
+    const num = 10
+    const PI = 3.14
+    console.log(`값은 ${num}이고, 타입은 ${typeof num}입니다`)
+    console.log(`값은 ${PI}이고, 타입은 ${typeof PI}입니다`)
+
+    const a = 1 / 0
+    const b = -1 / 0
+    const c = 'html' / 0
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+
+}
+
+// 불(불린, 불리언) Boolean
+{
+    const a = true
+    const b = false
+
+    const name = 'Alice'
+    const age = 16
+
+    console.log(name == 'Alice')
+    console.log(age > 20)
+}
+
+//null - 존재하지 않는 값
+//underfined - 값이 항당되지 않음
+
+{
+
+    const fall = null 
+    let winter
+
+}
+
+//5. 대화상자 alert, prompt, confirm
+{
+//   const name = prompt('이름을 입력하세요', '영업팀')
+//     alert(`안녕하세요, ${name}님, 환영합니다 `)
+    const isCheck = confirm('확인하였습니까?')
+    console.log(isCheck)
+}
+
+
+//6. 형변환
+{
+    const mathScore = prompt('수학점수는?')
+    const engScore  = prompt('영엉점수는?')
+    const score = (mathScore + engScore) / 2
+ b
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/basic/app.js
@@ -0,0 +1,6 @@
+
+document.write('안녕')
+alert('안녕')
+
+
+document.querySelector('.tilte')
